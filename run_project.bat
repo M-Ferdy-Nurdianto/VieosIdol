6102255249653db20da2c889fc3969bd0410c199
M-Ferdy-Nurdianto <orangnoob6@gmail.com>
2026-05-08 12:05:23 +0700

    chore: update run_project.bat to auto-open browser pages

--- run_project.bat
+++ run_project.bat
@@ -4,5 +4,13 @@ echo Starting Server...
 start cmd /k "cd /d %~dp0server && npm run dev"
 echo Starting Client...
 start cmd /k "cd /d %~dp0client && npm run dev"
-echo Project is starting in separate windows.
+
+echo Menunggu Vite berjalan...
+timeout /t 5 /nobreak > NUL
+
+echo Membuka browser (Halaman User dan Admin)...
+start http://localhost:5173
+start http://localhost:5173/admin
+
+echo Project is starting in separate windows and browser.
 pause
